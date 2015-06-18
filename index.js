2c46e615f439784fea23987e3d7432d9d96cd6c2
Abhishek Pandey <enitihas@gmail.com>
2015-06-18 14:24:13 +0530

    Emit messages to everyone

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ app.get('/', function(req, res) {
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
+    // console.log('message: ' + msg);
+    io.emit('chat message', msg);
   });
 });
 
