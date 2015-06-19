3f7f03664a458552aa4616fa1e42f8856e192f40
Abhishek Pandey <enitihas@gmail.com>
2015-06-19 22:47:43 +0530

    Now it somewhat works . Need to implement a better UI and some more things.

--- index.js
+++ index.js
@@ -7,14 +7,41 @@ var httpServer = http.Server(app);
 var socketIO = require('socket.io');
 var io = socketIO(httpServer);
 
+var freeUsers = [];
+idmap = {};
+
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+	// console.log('New user joined' + socket.id);
+	if(freeUsers.length) {
+		var st1 = freeUsers.shift();
+		idmap[socket.id] = st1;
+		idmap[st1] = socket.id;
+		console.log('Conncected' + socket.id + '  to  ' + st1);
+	}
+	else {
+		freeUsers.push(socket.id);
+	}
+	socket.on('disconnect', function() {
+		var st2 = idmap[socket.id];
+		if(st2) {
+			delete idmap[socket.id];
+			delete idmap[st2];
+			if(freeUsers.length) {
+				var st3 = freeUsers.shift();
+				idmap[st2] = st3;
+				idmap[st3] = st2;
+			}
+			else freeUsers.push(st2);
+		}
+	});
   socket.on('chat message', function(msg){
-    // console.log('message: ' + msg);
-    io.emit('chat message', msg);
+     console.log('message: ' + msg);
+   if(idmap[socket.id]) io.to(idmap[socket.id]).emit('chat message', msg);
+   console.log(idmap);
   });
 });
 
